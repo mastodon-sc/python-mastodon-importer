54efbcb79e2092fcb759b2163934b87fc1605d87
Christoph Sommer <christoph.sommer@ist.ac.at>
2021-01-25 15:30:19 +0100

    first working version for reading tags

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Mastodon Reader for Python
-no tags/features import is implemented, yet
+no features import is implemented, yet
 
 ported from: https://github.com/mastodon-sc/matlab-mastodon-importer
 
 # Example
+Read the mastodon file:
 ```python
 from mastodon_reader import MastodonReader
 
 mr = MastodonReader("path/to/file.mastodon")
 
 # show meta data
-print(mr.read_metadata())
+mr.read_metadata()
 
-# get Graph (pandas tables)
-V, E = mr.read_graph()
+# read (networkX) Graph representation, Nodes and Edge tables, and the tag set definitions
+Graph, Nodes, Edges, TagSets = mr.read()
 
-# get networkx Graph
-G = mr.read_graph_networkx()
+```
+or read information separately
+
+```python
+# read only Nodes and Edge tables
+Nodes, Edges = mr.read_tables()
+
+# Read Tags and add found tags as columns to the Nodes and Edge tables
+TagSets = mr.read_tags(Nodes, Edges)
+
+# Create netwworkX Graph DiGraph representation
+Graph = mr.create_nx_graph(Nodes, Edges)
 ```
--- mastodon_reader/mastodon_reader.py
+++ mastodon_reader/mastodon_reader.py
@@ -7,6 +7,91 @@ import xml.etree.ElementTree as ET
 
 SUPPORTED_MASTODON_VERSIONS = ["0.3"]
 
+import numpy as np
+
+# %% ismember
+def ismember(a_vec, b_vec, method=None):
+    """
+
+    Description
+    -----------
+    MATLAB equivalent ismember function
+    [LIA,LOCB] = ISMEMBER(A,B) also returns an array LOCB containing the
+    lowest absolute index in B for each element in A which is a member of
+    B and 0 if there is no such index.
+    Parameters
+    ----------
+    a_vec : list or array
+    b_vec : list or array
+    method : None or 'rows' (default: None).
+        rows can be used for row-wise matrice comparison.
+    Returns an array containing logical 1 (true) where the data in A is found
+    in B. Elsewhere, the array contains logical 0 (false)
+    -------
+    Tuple
+
+    Example
+    -------
+    >>> a_vec = np.array([1,2,3,None])
+    >>> b_vec = np.array([4,1,2])
+    >>> Iloc,idx = ismember(a_vec,b_vec)
+    >>> a_vec[Iloc] == b_vec[idx]
+
+    """
+    # Set types
+    a_vec, b_vec = _settypes(a_vec, b_vec)
+
+    # Compute
+    if method is None:
+        Iloc, idx = _compute(a_vec, b_vec)
+    elif method == "rows":
+        if a_vec.shape[0] != b_vec.shape[0]:
+            raise Exception("Error: Input matrices should have same number of columns.")
+        # Compute row-wise over the matrices
+        out = list(map(lambda x, y: _compute(x, y), a_vec, b_vec))
+        # Unzipping
+        Iloc, idx = list(zip(*out))
+    else:
+        Iloc, idx = None, None
+
+    return (Iloc, idx)
+
+
+# %% Compute
+def _settypes(a_vec, b_vec):
+    if "pandas" in str(type(a_vec)):
+        a_vec.values[np.where(a_vec.values == None)] = "NaN"
+        a_vec = np.array(a_vec.values)
+    if "pandas" in str(type(b_vec)):
+        b_vec.values[np.where(b_vec.values == None)] = "NaN"
+        b_vec = np.array(b_vec.values)
+    if isinstance(a_vec, list):
+        a_vec = np.array(a_vec)
+        # a_vec[a_vec==None]='NaN'
+    if isinstance(b_vec, list):
+        b_vec = np.array(b_vec)
+        # b_vec[b_vec==None]='NaN'
+
+    return a_vec, b_vec
+
+
+# %% Compute
+def _compute(a_vec, b_vec):
+    bool_ind = np.isin(a_vec, b_vec)
+    common = a_vec[bool_ind]
+    [common_unique, common_inv] = np.unique(common, return_inverse=True)
+    [b_unique, b_ind] = np.unique(b_vec, return_index=True)
+    common_ind = b_ind[np.isin(b_unique, common_unique, assume_unique=True)]
+
+    return bool_ind, common_ind[common_inv]
+
+
+def RGBint2RGB(rgb_int):
+    B = rgb_int & 255
+    G = (rgb_int >> 8) & 255
+    R = (rgb_int >> 16) & 255
+    return (R, G, B)
+
 
 class JavaRawReader:
     MAGIC = -21267
@@ -37,6 +122,17 @@ class JavaRawReader:
         self.index += size
 
         return res
+
+    def read_int(self):
+        return self.read(4, ">i")[0]
+
+    def read_short(self):
+        return self.read(2, ">h")[0]
+
+    def read_utf8(self):
+        str_length = self.read_short()
+
+        return b"".join(self.read(str_length, ">" + "c" * str_length)).decode("utf-8")
 
     def _fetch_block(self):
         block_key = struct.unpack(">b", self._fh.read(1))[0]
@@ -84,30 +180,164 @@ class MastodonReader:
             "time unit": time_units,
         }
 
-    def read_tags(self):
-        pass
+    def read_tags(self, V, E):
+        with zipfile.ZipFile(self.source_file) as masto_zip:
+            fh = masto_zip.open("tags.raw", "r")
+            jr = JavaRawReader(fh)
+
+            tss = self._read_tag_set_structure(jr)
+
+            label_sets_vertices, map_vertices = self._read_label_set_property_map(jr)
+            label_sets_edges, map_edges = self._read_label_set_property_map(jr)
+
+            self._append_tags_to_table(map_vertices, label_sets_vertices, tss, V)
+            self._append_tags_to_table(map_edges, label_sets_edges, tss, E)
+
+            return tss
+
+    def _append_tags_to_table(self, map_, label_sets, tss, tab):
+        # Prepare columns.
+        n_tag_set = len(tss)
+        columns = {}
+        for i in range(n_tag_set):
+            columns[i] = numpy.zeros(len(tab), dtype="int32")
+            columns[i].fill(-1)
+
+        # Map tag ids to tag_set.
+        n_total_tags = 0
+        for ts in tss:
+            n_total_tags = n_total_tags + len(ts["tags"])
+
+        tag_map = {}
+        tag_set_map = {}
+
+        for i in range(len(tss)):
+            for j in range(len(tss[i]["tags"])):
+                tag_map[tss[i]["tags"][j]["id"]] = tss[i]["tags"][j]["label"]
+                tag_set_map[tss[i]["tags"][j]["id"]] = i  ###
+
+        n_label_sets = len(label_sets)
+
+        #  Process label-set by label-set.
+        for i in range(n_label_sets):
+
+            label_set = label_sets[i]
+            n_labels = len(label_set)
+
+            for j in range(n_labels):
+
+                # What is the tag-id of this element in the label set?
+                tag_id = label_set[j]
+
+                # What tag-set column are we editing for this tag_id?
+                tag_set = tag_set_map[tag_id]
+
+                # % What rows, in the map, have this label-set?
+                idx2 = numpy.nonzero(map_[:, 1] == i)[0]  # orig: ( i - 1  ) ); % 1 -> 0
+
+                #  What object ids correspond to these rows?
+                object_ids = map_[idx2, 0]
+
+                # % What are the rows, in the table, that have these ids?
+
+                _, idx1 = ismember(object_ids, tab.index.to_numpy())
+
+                #% Fill these rows with the tag_id
+                columns[tag_set][idx1] = tag_id
+
+        for i, ts in enumerate(tss):
+            tab[ts["name"] + "_ID"] = columns[i]
+            tab[ts["name"] + "_NAME"] = ""
+
+            tab[ts["name"] + "_NAME"] = tab[ts["name"] + "_ID"].apply(
+                lambda xxx: tag_map[xxx] if xxx > -1 else ""
+            )
+
+        return tss
+
+    def _read_label_set_property_map(self, jr):
+        num_sets = jr.read_int()
+        label_sets = []
+
+        for i in range(num_sets):
+            num_labels = jr.read_int()
+            labels = numpy.zeros(num_labels, dtype="int32")
+
+            for j in range(num_labels):
+                # The labels are ints in Mastodon -> tag linear index
+                labels[j] = jr.read_int()
+
+            label_sets.append(labels)
+
+        # Read entries.
+        size = jr.read_int()
+        map_ = numpy.zeros((size, 2), dtype="int32")
+
+        for i in range(size):
+            map_[i, 0] = jr.read_int()
+            map_[i, 1] = jr.read_int()
+
+        return label_sets, map_
+
+    def _read_tag_set_structure(self, jr):
+        n_tag_sets = jr.read_int()
+
+        tag_sets = []
+
+        for _ in range(n_tag_sets):
+            tag_set_id = jr.read_int()
+            tag_set_name = jr.read_utf8()
+            n_tags = jr.read_int()
+
+            tag_set = {"name": tag_set_name, "label": tag_set_id}
+
+            tags = []
+            for _ in range(n_tags):
+                tag_id = jr.read_int()
+                tag_label = jr.read_utf8()
+                tag_color = jr.read_int()
+                tag = {
+                    "id": tag_id,
+                    "label": tag_label,
+                    "color": tag_color,
+                    "color_rgb": RGBint2RGB(tag_color),
+                }
+                tags.append(tag)
+
+            tag_set["tags"] = tags
+
+            tag_sets.append(tag_set)
+
+        return tag_sets
 
     def read_features(self):
         pass
 
-    def read_graph_networkx(self):
+    def create_nx_graph(self, V, E):
         import networkx as nx
 
-        V, E = self.read_graph()
-
-        G = nx.from_pandas_edgelist(E, source="source_idx", target="target_idx")
+        G = nx.from_pandas_edgelist(
+            E, source="source_idx", target="target_idx", create_using=nx.DiGraph
+        )
         nx.set_node_attributes(G, V.to_dict("index"))
 
         return G
 
-    def read_graph(self):
+    def read(self):
+        V, E = self.read_tables()
+        tss = self.read_tags(V, E)
+        G = self.create_nx_graph(V, E)
+
+        return G, V, E, tss
+
+    def read_tables(self):
         with zipfile.ZipFile(self.source_file) as masto_zip:
             fh = masto_zip.open("model.raw", "r")
 
             jr = JavaRawReader(fh)
 
             try:
-                n_verticices = jr.read(4, ">i")[0]
+                n_verticices = jr.read_int()
                 V = numpy.zeros(shape=(n_verticices, 11), dtype=numpy.float32)
 
                 for i in range(n_verticices):
@@ -132,7 +362,7 @@ class MastodonReader:
                     ],
                 )
 
-                n_edges = jr.read(4, ">i")[0]
+                n_edges = jr.read_int()
                 E = numpy.zeros((n_edges, 3), dtype=numpy.int32)
 
                 for i in range(n_edges):
